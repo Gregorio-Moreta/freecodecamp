262d71105f6f880fb9aae1a8c5e8a16f62099f1e
Gregorio-Moreta <78994816+Gregorio-Moreta@users.noreply.github.com>
2023-08-02 11:26:16 -0400

    switch cases!

--- basicJavascript/golf.js
+++ basicJavascript/golf.js
@@ -1,7 +1,8 @@
 // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/golf-code
-// Question
 
+// Question
 // Golf Code
+
 // In the game of Golf, each hole has a par, meaning, the average number of strokes a golfer is expected to make in order to sink the ball in the hole to complete the play. Depending on how far above or below par your strokes are, there is a different nickname.
 
 // Your function will be passed par and strokes arguments. Return the correct string according to this table which lists the strokes in order of priority; top (highest) to bottom (lowest):
